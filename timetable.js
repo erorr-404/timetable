d5a1e377fbc59b6a612eeb660cde9d869f22e6ee
Erorr <maxymholub@gmail.com>
2024-09-06 15:00:05 +0300

    Finished base of timetable

--- timetable.js
+++ timetable.js
@@ -1,9 +1,9 @@
+const table = document.getElementById("main-table")
+const tableRows = table.querySelectorAll("tr")
+
 fetch("./data/timetable.json")
   .then((response) => response.json())
   .then((json) => {
-    const table = document.getElementById("main-table")
-    const tableRows = table.querySelectorAll("tr")
-
     for (const [day, subjects] of Object.entries(json)) {
       let counter = 1
       subjects.forEach(subject => {
@@ -13,4 +13,24 @@ fetch("./data/timetable.json")
         counter++
       })
     }
-})
+
+    for (let i = 1; i < 8; i++) {
+      const cell = document.createElement("td")
+      cell.innerText = i
+      tableRows[i].appendChild(cell)
+    }
+  })
+
+fetch("./data/time.json")
+  .then((response) => response.json())
+  .then((json) => {
+    for (const [number, time] of Object.entries(json)) {
+      const startCell = document.createElement("td")
+      startCell.innerText = `${time.start.hour}:${time.start.minute}`
+      tableRows[number].appendChild(startCell)
+
+      const endCell = document.createElement("td")
+      endCell.innerText = `${time.end.hour}:${time.end.minute}`
+      tableRows[number].appendChild(endCell)
+    }
+  })
