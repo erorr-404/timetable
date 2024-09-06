01d52c99bb1d96f74b31f5df85f71d1c33b5abf8
Erorr <maxymholub@gmail.com>
2024-09-06 16:28:49 +0300

    Added lessons importance color

--- timetable.js
+++ timetable.js
@@ -1,5 +1,12 @@
 const table = document.getElementById("main-table")
 const tableRows = table.querySelectorAll("tr")
+let importance = {}
+
+fetch("./data/importance.json")
+  .then((response) => response.json())
+  .then((json) => {
+    importance = json
+  })
 
 fetch("./data/timetable.json")
   .then((response) => response.json())
@@ -9,6 +16,7 @@ fetch("./data/timetable.json")
       subjects.forEach(subject => {
         const cell = document.createElement("td")
         cell.innerText = subject
+        cell.classList.add(`imp-lvl-${importance[subject]}`)
         tableRows[counter].appendChild(cell)
         counter++
       })
@@ -17,6 +25,7 @@ fetch("./data/timetable.json")
     for (let i = 1; i < 8; i++) {
       const cell = document.createElement("td")
       cell.innerText = i
+      cell.classList.add("text-center")
       tableRows[i].appendChild(cell)
     }
   })
@@ -27,10 +36,12 @@ fetch("./data/time.json")
     for (const [number, time] of Object.entries(json)) {
       const startCell = document.createElement("td")
       startCell.innerText = `${time.start.hour}:${time.start.minute}`
+      startCell.classList.add("text-center")
       tableRows[number].appendChild(startCell)
 
       const endCell = document.createElement("td")
       endCell.innerText = `${time.end.hour}:${time.end.minute}`
+      endCell.classList.add("text-center")
       tableRows[number].appendChild(endCell)
     }
   })
