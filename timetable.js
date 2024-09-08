be85176c0b0f9e2c3dbe7325dee846962ee74ce3
Erorr <maxymholub@gmail.com>
2024-09-08 10:22:46 +0300

    Fixed empty timetable bug

--- timetable.js
+++ timetable.js
@@ -1,63 +1,64 @@
 const table = document.getElementById("main-table")
 const tableRows = table.querySelectorAll("tr")
-let importance = {}
-let links = {}
 
-fetch("./data/importance.json")
-  .then((response) => response.json())
-  .then((json) => {
-    importance = json
+const fetchRequests = [
+  fetch("./data/importance.json"),
+  fetch("./data/links.json"),
+  fetch("./data/timetable.json"),
+  fetch("./data/time.json")
+]
+
+Promise.all(fetchRequests)
+  .then(response => {
+    return Promise.all(response.map(response => response.json()))
+  })
+  .then(data => {
+    updateTable(data)
+  })
+  .catch(error => {
+    alert(`Error: ${error}`)
   })
 
-  fetch("./data/links.json")
-  .then((response) => response.json())
-  .then((json) => {
-    links = json
-  })
+function updateTable(data) {
+  const dataImportance = data[0]
+  const dataLinks = data[1]
+  const dataTimetable = data[2]
+  const dataTime = data[3]
 
-fetch("./data/timetable.json")
-  .then((response) => response.json())
-  .then((json) => {
-    for (const [day, subjects] of Object.entries(json)) {
-      let counter = 1
-      subjects.forEach(subject => {
-        while (importance[subject] == undefined) {}
-        const cell = document.createElement("td")
-        cell.classList.add(`imp-lvl-${importance[subject]}`)
-
-        const link = document.createElement("a")
-        while (links[subject] == undefined) {}
-        link.href = links[subject]
-        link.target = "_blank"
-        link.innerText = subject
-
-        cell.appendChild(link)
-
-        tableRows[counter].appendChild(cell)
-        counter++
-      })
-    }
-
-    for (let i = 1; i < 8; i++) {
+  for (const [day, subjects] of Object.entries(dataTimetable)) {
+    let counter = 1
+    subjects.forEach(subject => {
       const cell = document.createElement("td")
-      cell.innerText = i
-      cell.classList.add("text-center")
-      tableRows[i].appendChild(cell)
-    }
+      cell.classList.add(`imp-lvl-${dataImportance[subject]}`)
 
-    fetch("./data/time.json")
-    .then((response) => response.json())
-    .then((json) => {
-      for (const [number, time] of Object.entries(json)) {
-        const startCell = document.createElement("td")
-        startCell.innerText = `${time.start.hour}:${time.start.minute}`
-        startCell.classList.add("text-center")
-        tableRows[number].appendChild(startCell)
+      const link = document.createElement("a")
+      link.href = dataLinks[subject]
+      link.target = "_blank"
+      link.innerText = subject
 
-        const endCell = document.createElement("td")
-        endCell.innerText = `${time.end.hour}:${time.end.minute}`
-        endCell.classList.add("text-center")
-        tableRows[number].appendChild(endCell)
-      }
+      cell.appendChild(link)
+
+      tableRows[counter].appendChild(cell)
+      counter++
     })
-  })
+  }
+
+  for (let i = 1; i < 8; i++) {
+    const cell = document.createElement("td")
+    cell.innerText = i
+    cell.classList.add("text-center")
+    tableRows[i].appendChild(cell)
+  }
+
+  for (const [number, time] of Object.entries(dataTime)) {
+    const startCell = document.createElement("td")
+    startCell.innerText = `${time.start.hour}:${time.start.minute}`
+    startCell.classList.add("text-center")
+    tableRows[number].appendChild(startCell)
+
+    const endCell = document.createElement("td")
+    endCell.innerText = `${time.end.hour}:${time.end.minute}`
+    endCell.classList.add("text-center")
+    tableRows[number].appendChild(endCell)
+  }
+}
